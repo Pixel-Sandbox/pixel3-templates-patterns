0e30caa54ce85e8ba5813e2f1b55af1f43405f66
--global <--global>
2024-12-18 06:59:08 +0000

    Sync pattern-templates folder to GitHub Source Commit : http://bitbucket.org/mid-kelola-indonesia/pixel3/commits/1953ad9c48e0534c590bf6cba23d7111080e5c55

---
+++ src/templates/layouts/general/pages/child-menu.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { MpFlex, MpText, css } from '@mekari/pixel3'
+import { usePixelLayout } from '@/components/layouts/composables'
+import BaseLayout from '../parts/BaseLayout.vue'
+
+const { pixelContentAttrs } = usePixelLayout()
+
+const mainContentClass = css({
+  bg: 'background.neutral',
+  borderTopWidth: '1px',
+  borderLeftWidth: '1px',
+  borderColor: 'border.default',
+  roundedTopLeft: 'md',
+  p: 6,
+  minH: '100svh'
+})
+</script>
+
+<template>
+  <BaseLayout :isShowChildMenu="true">
+    <main data-pixel-component="PixelContent" v-bind="pixelContentAttrs">
+      <MpFlex justify="space-between" align-items="center" px="6" py="1.063rem">
+        <MpText size="h1" weight="semiBold"> Title </MpText>
+      </MpFlex>
+
+      <div :class="mainContentClass">Content</div>
+    </main>
+  </BaseLayout>
+</template>
+
+<route lang="yaml">
+meta:
+  cover: /cover/templates/layouts/default.png
+</route>
