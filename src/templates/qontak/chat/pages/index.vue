26058d7572a55c8e1ccc27b1e01edbca16b78697
--global <--global>
2025-05-08 07:05:56 +0000

    Sync pattern-templates folder to GitHub Source Commit : http://bitbucket.org/mid-kelola-indonesia/pixel3/commits/9c540116fc1b42657abbecf9db8ec8421c20e07e

---
+++ src/templates/qontak/chat/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+  router.push('/templates/qontak/chat/inbox')
+</script>
+
+<route lang="yaml">
+meta:
+  cover: /cover/templates/qontak/inbox-revamp.jpg
+</route>
